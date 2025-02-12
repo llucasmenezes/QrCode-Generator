f6cbb411d1dbd713f1409edb2bfc72d7c16be9f4
llucasmenezes <llucasmenezes99@gmail.com>
2025-02-11 21:07:19 -0300

    feat: implementando QrCodeService

--- src/main/java/com/qrcode/Generator/service/QrCodeService.java
+++ src/main/java/com/qrcode/Generator/service/QrCodeService.java
@@ -3,38 +3,43 @@ package com.qrcode.Generator.service;
 import com.qrcode.Generator.model.QrCode;
 import com.qrcode.Generator.repository.QrCodeRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import net.glxn.qrgen.javase.QRCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import net.glxn.qrgen.javase.QRCode;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Service
 public class QrCodeService {
 
     @Autowired
     private QrCodeRepository qrCodeRepository;
 
+    // Método para gerar QR Code e retornar a imagem como um array de bytes
+    public QrCode GerarQrCode(String url) {
+        try {
 
-    public QrCode GerarQrCode(String url){
-        try{
-            String caminhoDoArquivo = System.getProperty("user.home") + "\\Downloads\\qr_romantico.png";
-            File file = new File(caminhoDoArquivo);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            QRCode.from(url).withSize(300, 300).writeTo(byteArrayOutputStream);
+            byte[] qrCodeBytes = byteArrayOutputStream.toByteArray();
 
-            FileOutputStream fos = new FileOutputStream(file);
-            fos.write(QRCode.from(url).withSize(300, 300).stream().toByteArray());
-            fos.close();
 
-            QrCode qrCode = new QrCode(url, "/qr_romantico.png");
-            return qrCodeRepository.save(qrCode);
+            QrCode qrCode = new QrCode(url, "/qrcode_generator.png");
+            qrCodeRepository.save(qrCode);
 
-        }catch(Exception e){
+
+            return qrCode;
+
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    // Método para listar todos os QR Codes salvos no banco de dados
+    public List<QrCode> listarQrCodes() {
+        return qrCodeRepository.findAll();
     }
 }
